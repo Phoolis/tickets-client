f578fad3d8b591c82e1722fda17541c46b755f3f
Phoolis <carlsonps@gmail.com>
2024-11-07 20:59:52 +0200

    Add markTicketAsUsed

--- src/TicketScanner.jsx
+++ src/TicketScanner.jsx
@@ -5,33 +5,44 @@ export default function TicketScanner() {
   const [ticketData, setTicketData] = useState(null);
   const [barcode, setBarcode] = useState("");
 
-  let url = "http://localhost:8080/api/tickets";
+  const url = "http://localhost:8080/api/tickets";
   //"https://ticketguru.hellmanstudios.fi/api/tickets/1";
 
   const username = "admin@test.com";
   const password = "admin";
   const authToken = btoa(`${username}:${password}`);
 
+  // add basic auth header to all axios requests
   axios.defaults.headers.common["Authorization"] = `Basic ${authToken}`;
 
   const handleChange = (event) => {
     setBarcode(event.target.value);
   };
 
+  // fetch data on pressing Enter
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      fetchTicketData({ url, barcode });
-      setBarcode("");
+      fetchTicketData(barcode);
     }
   };
 
-  const fetchTicketData = async ({ url, barcode }) => {
+  const fetchTicketData = async (barcode) => {
     try {
       const response = await axios.get(url + "/barcode/" + barcode);
       setTicketData(response.data);
     } catch (error) {
-      console.error("Error fetching: ", error);
+      console.error("Error fetching ticket data: ", error);
+    }
+  };
+
+  const markTicketAsUsed = async (barcode) => {
+    try {
+      const response = await axios.put(url + "/use/" + barcode);
+      setTicketData(response.data);
+      setBarcode("");
+    } catch (error) {
+      console.error("Error marking ticket as used: ", error);
     }
   };
 
@@ -46,9 +57,14 @@ export default function TicketScanner() {
         onKeyDown={handleKeyPress}
       />
       {ticketData && (
-        <div>
-          <p>{JSON.stringify(ticketData)}</p>
-        </div>
+        <>
+          <div>
+            <p>{JSON.stringify(ticketData)}</p>
+          </div>
+          <button onClick={() => markTicketAsUsed(barcode)}>
+            Mark as Used
+          </button>
+        </>
       )}
     </div>
   );
